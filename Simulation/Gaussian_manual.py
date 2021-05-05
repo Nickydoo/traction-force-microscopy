8eccb44201fc6e108c9a63b1d3cc8bb30df58b78
samuelb9 <samuel.brown@alumni.ubc.ca>
2021-05-05 16:30:37 -0400

    Small changes

--- Simulation/Gaussian_manual.py
+++ Simulation/Gaussian_manual.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.ndimage as nd
 import matplotlib.ticker as tkr
+import cv2
 np.random.seed(1)
 
 
@@ -86,41 +87,50 @@ def create_image(res, sf, make_plot=True, return_array=True, save_image=True):
         return mat, mat2
 
 
-def move_points(mat, move_by_px):
+def move_points(mat, move_by_px_up, move_by_px_down):
     """
-    :param positions: Matrix of positions
-    :param move_by_px: Amount to move each dot by
+    :param move_by_px_down: Amount to move each dot by in the down direction
+    :param mat: matrix
+    :param move_by_px_up: Amount to move each dot by in the up direction
     :return: Unfiltered array of moved points
     """
-    # positions = create_image(res, sf, make_plot=False, save_image=False, return_array=True)[0]
     positions = mat.copy()
     rows = cols = len(positions)
     quarter_size = int(rows / 4)
     second_quarter_start = 0 + quarter_size
     second_quarter_end = second_quarter_start + quarter_size
-    third_quarter_start = second_quarter_end
+    third_quarter_start = second_quarter_end + 1
     third_quarter_end = third_quarter_start + quarter_size
     for row in range(second_quarter_start, second_quarter_end):
         for col in range(cols):
             if positions[row][col] == 1:
-                positions[row - move_by_px][col] = 1
+                positions[row - move_by_px_up][col] = 1
                 positions[row][col] = 0
     for row in range(third_quarter_start, third_quarter_end):
         for col in range(cols):
             if positions[row][col] == 1:
-                positions[row + move_by_px][col] = 1
+                positions[row + move_by_px_down][col] = 1
                 positions[row][col] = 0
     return positions
 
 
 raw_mat = raw_points_grid(1608, 2)
-moved_mat = move_points(raw_mat, 100)
+moved_mat = move_points(raw_mat, move_by_px_up=100, move_by_px_down=0)
 rarrs = [raw_mat, moved_mat]
 unmoved_gauss = nd.gaussian_filter(raw_mat, sigma=5.0, order=0)
 moved_gauss = nd.gaussian_filter(moved_mat, sigma=5.0, order=0)
 gaussians = [unmoved_gauss, moved_gauss]
 make_plots(rarrs, gaussians, 2)
 
+raw_diffs = cv2.subtract(moved_mat, raw_mat)
+plt.imshow(raw_diffs, cmap="gray")
+plt.show()
+mod_diffs = cv2.subtract(moved_gauss, unmoved_gauss)
+plt.imshow(mod_diffs, cmap="gray")
+plt.show()
+
+#TODO: compute error of localization - use local maxima detection
+#TODO: compute displacement & displacement error - reference 20.2.3 of plotnikov paper
 
 # fmt = tkr.FuncFormatter(numfmt)
 # resolution = 1608 * scale_factor
