8d62cd708cc2e1c0218953b96ac432a88ba9d936
samuelb9 <samuel.brown@alumni.ubc.ca>
2021-05-07 11:06:01 -0400

    First attempt implementing pkfind.m in python

--- Simulation/my_utils.py
+++ Simulation/my_utils.py
@@ -1,5 +1,6 @@
 import numpy as np
 from skimage import measure
+from math import floor
 
 np.random.seed(1)
 
@@ -55,7 +56,8 @@ def struct_sim(imageA, imageB):
     """
     return measure.compare_ssim(imageA, imageB)
 
-def pkfnd(im, th, sz):
+
+def pkfnd(im, th, sz=None):
     """
     finds local maxima in an image to pixel level accuracy
     provides a rough guess of particle centers
@@ -68,8 +70,8 @@ def pkfnd(im, th, sz):
     out[:, 2] are the y coordinates of the maxima
     Adapted from Eric R. Dufresne MATLAB code
     """
-
-    ind = np.argwhere(im > th)
+    t = 1.5  # scaling factor for threshold
+    ind = np.argwhere(im > t * th)
     nr, nc = im.shape
     tst = np.zeros((nr, nc))
     n = len(ind)
@@ -78,11 +80,49 @@ def pkfnd(im, th, sz):
         return []
     mx = []
     # convert index from find to row and column
-    rc = [ind % nr, (ind/nr).floor()+1]
-    for i in range(n):
+    rc = [ind % nr, floor(ind / nr) + 1]
+    for i in range(n):  # this might have to be n+1, check later
         r = rc[i, 0]
         c = rc[i, 1]
         # check each pixel above threshold to see if it's brighter than
         # its neighbors
-        # if 1 < r < nr and 1 < c < nc:
-            # TODO finish implementing
+        if 1 < r < nr and 1 < c < nc:
+            if im[r, c] >= im[r - 1, c - 1] and im[r, c] >= im[r, c - 1] and im[r, c] >= im[r + 1, c - 1] and im[
+                r, c] >= im[r - 1, c] & im[r, c] >= im[r + 1, c] and im[r, c] >= im[r - 1, c + 1] and im[r, c] >= im[
+                r, c + 1] and im[r, c] >= im[r + 1, c + 1]:
+                mx = np.concat([mx, np.concat([r, c]).T])
+    mx = mx.T
+    npks, crap = mx.shape
+    # if size is specified, then get rid of pks within size of boundary
+    if sz is not None and npks > 0:
+        # throw out all pks within sz of boundary
+        ind = np.argwhere(sz < mx[:, 0] < (nr - sz) and sz < mx[:, 1] < (nc - sz))
+        mx = mx[ind, :]
+
+    npks, crap = mx.shape
+    if npks > 1:
+        # create an image with only peaks
+        nmx = npks
+        tmp = 0 * im
+        for i in range(nmx):  # might have to be nmx + 1
+            tmp[mx[i, 0], mx[i, 1]] = im[mx[i, 0], mx[i, 1]]
+        # look in neighborhood around each peak, pick the brightest
+        for i in range(nmx):  # might have to be nmx + 1
+            roi = tmp[[mx[i, 0] - floor(sz / 2)]:[mx[i, 0] + (floor(sz / 2) + 1)], (mx(i, 1) - floor(sz / 2)): (
+                    mx[i, 1] + (floor(sz / 2) + 1))]
+            mv, indi = np.max(roi)
+            mv, indj = np.max(mv)
+            tmp[(mx[i, 0] - floor(sz / 2)): (mx[i, 0] + (floor(sz / 2) + 1)), (mx[i, 1] - floor(sz / 2)): (
+                    mx[i, 1] + (floor(sz / 2) + 1))] = 0
+            tmp[mx[i, 0] - floor(sz / 2) + indi[indj] - 1, mx[i, 1] - floor(sz / 2) + indj - 1] = mv
+        ind = np.argwhere(tmp > 0)
+        mx = [ind % nr, floor(ind/nr) + 1]
+    if mx.shape == (0, 0):
+        return []
+    else:
+        out = []
+        out[:, 1] = mx[:, 0]
+        out[:, 0] = mx[:, 1]
+        return out
+
+
