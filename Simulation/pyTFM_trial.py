56ec27b0523bf18b80b5b36164edb29c6a63b65c
Sam Brown <samuel.brown@alumni.ubc.ca>
2021-05-27 14:51:23 -0400

    Added some functions for strain energy

--- Simulation/pyTFM_trial.py
+++ Simulation/pyTFM_trial.py
@@ -1,33 +1,27 @@
-from my_utils import shift_by_one, move_points_circle, raw_points_grid, raw_points_grid_and_x_y, x_y_to_img
+from my_utils import move_points_circle, raw_points_grid_and_x_y, x_y_to_img
+from calculate_traction import reg_fttc
 from scipy.ndimage import gaussian_filter
 from openpiv.pyprocess import extended_search_area_piv, get_coordinates
 from openpiv.validation import sig2noise_val
 from openpiv.filters import replace_outliers
 from plotting import show_quiver
 import matplotlib.pyplot as plt
+from synthetic_image import gen_img
 import numpy as np
 
 
-# generate synthetic images
-
-# moved_right = np.roll(raw_before[0:201, 0:201], 5)
-# raw_moved = raw_before.copy()
-# raw_moved[0:201, 0:201] = moved_right
-def make_images():
-    raw_before, raw_x, raw_y = raw_points_grid_and_x_y(1608, 1)
-    moved_x, moved_y, displacement_x, displacement_y = move_points_circle(raw_x, raw_y, 150)
-    moved_x_r = [int(round(num)) for num in moved_x]
-    moved_y_r = [int(round(num)) for num in moved_y]
-    raw_moved = x_y_to_img(moved_x_r, moved_y_r)
-    gaussian_before = gaussian_filter(raw_before, sigma=5, order=0)
-    gaussian_after = gaussian_filter(raw_moved, sigma=5, order=0)
-    return gaussian_before, gaussian_after, displacement_x, displacement_y
-
-
-def get_displacements(img1, img2, window_size=64):
-    u, v, sig2noise = extended_search_area_piv(img1, img2, window_size=window_size, overlap=window_size // 2, dt=1,
+def get_displacements(img1, img2, overlap: int, window_size: int, normalized: bool = True):
+    """
+    :param img1: before image - image with cell on substrate
+    :param img2: after image - image after cell has been removed
+    :param overlap: how much the cross correlation windows will overlap (must be > window_size/2)
+    :param window_size: size of search windows for cross correlation
+    :param normalized: Whether to normalize search windows, True is more accurate but takes longer
+    :return: dictionary with x,y coordinates of search windows and u (x-component) and v (y-component) of displacement
+    """
+    u, v, sig2noise = extended_search_area_piv(img1, img2, window_size=window_size, overlap=overlap, dt=1,
                                                subpixel_method="gaussian", search_area_size=window_size,
-                                               sig2noise_method="peak2peak")
+                                               sig2noise_method="peak2peak", normalized_correlation=normalized)
 
     u, v, mask = sig2noise_val(u, v, sig2noise, threshold=1.05)
 
@@ -40,27 +34,59 @@ def get_displacements(img1, img2, window_size=64):
     v[mask_std] = np.nan
 
     u, v = replace_outliers(u, v, method="localmean", max_iter=10, kernel_size=2)
-    x, y = get_coordinates(image_size=img1.shape, search_area_size=window_size, overlap=window_size // 2)
+    x, y = get_coordinates(image_size=img1.shape, window_size=window_size, overlap=overlap)
     y = y[::-1]
     v = -v
-    return {'x': x, 'y': y, 'u': u, 'v': v}
-im_before = raw_points_grid(1608, 1)
-im_after = np.roll(np.roll(im_before.copy(), 1, axis=0),1, axis=1)
-for wsize in range(32, 16*10+1, 16):
-    gauss_before = gaussian_filter(im_before, sigma=5)
-    gauss_after = gaussian_filter(im_after, sigma=5)
-    disps = get_displacements(gauss_before, gauss_after, window_size=wsize)
-    u, v = disps["u"], disps["v"]
-    actual_x_disps = np.full(u.shape, 1)
-    actual_y_disps = np.full(v.shape, 1)
-    xerror = ((actual_x_disps - u)**2)
-    yerror = ((actual_y_disps - u)**2)
-    mxerror = np.average(xerror)
-    myerror = np.average(yerror)
-    mse = np.average(np.stack((mxerror, myerror)))
-    plt.plot(u, v, 'r.')
-    plt.show()
-    print(f'relative error for window size {wsize} is {1/ mse * 100} % ')
+    return {'x': x, 'y': y, 'u': u, 'v': v, 'mask': mask, 'mask_std': mask_std}
+
+
+def strain_energy_points(u, v, tx, ty, pix):
+    """
+    :param u: displacement field in x direction
+    :param v: displacement field in y direction
+    :param tx: tractions in x direction
+    :param ty: tractions in y direction
+    :param pix: pixel-distance factor
+    :return: pointwise energy (use total_strain_energy for magnitude)
+    """
+    pix *= 10e-6
+    energy = ((pix ** 2) / 2) * (tx * u * pix + ty * v * pix)
+    return energy
+
+
+def total_strain_energy(strain_energy, mask):
+    """
+    Calculate total strain energy
+    :param strain_energy: from strain_energy_points
+    :param mask: mask of cell boundary
+    :return: total strain energy
+    """
+    return np.sum(strain_energy[mask])
+
+
+im_before, im_after = gen_img(1608, 0.25, 100, 100, 20)
+disps = get_displacements(im_before, im_after, window_size=64)
+u, v = disps['u'], disps['v']
+tx, ty = reg_fttc(u, v, 10e-4, 4000, 0.49, pix=120000, regularized=False)
+print(tx.shape)
+fig, ax = show_quiver(tx, ty)
+plt.show()
+
+# for wsize in range(32, 16*10+1, 16):
+#     gauss_before = gaussian_filter(im_before, sigma=5)
+#     gauss_after = gaussian_filter(im_after, sigma=5)
+#     disps = get_displacements(gauss_before, gauss_after, window_size=wsize)
+#     u, v = disps["u"], disps["v"]
+#     actual_x_disps = np.full(u.shape, 1)
+#     actual_y_disps = np.full(v.shape, 1)
+#     xerror = ((actual_x_disps - u)**2)
+#     yerror = ((actual_y_disps - u)**2)
+#     mxerror = np.average(xerror)
+#     myerror = np.average(yerror)
+#     mse = np.average(np.stack((mxerror, myerror)))
+#     plt.plot(u, v, 'r.')
+#     plt.show()
+#     print(f'relative error for window size {wsize} is {1/ mse * 100} % ')
 
 
 # im_before, im_after, x_movements, y_movements = make_images()
