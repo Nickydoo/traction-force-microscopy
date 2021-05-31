bbaf0c057609e260ffb425f0b6c2b288c0a6b179
Sam Brown <samuel.brown@alumni.ubc.ca>
2021-05-31 16:09:11 -0400

    Update main.py

--- Simulation/main.py
+++ Simulation/main.py
@@ -1,22 +1,35 @@
 import numpy as np
 import matplotlib.pyplot as plt
-# from openpiv.pyprocess import extended_search_area_piv, get_coordinates
-# from openpiv.validation import sig2noise_val
-# from openpiv.filters import replace_outliers
 from calculate_traction import reg_fttc
-from pyTFM_trial import get_displacements
+from pyTFM_trial import get_displacements, strain_energy_points
 from plotting import show_quiver
+import cv2
 
 
-before_image_path = r"C:\Users\sbrown\Documents\GitHub\traction-force-microscopy\Data\C1-middle well001.png"
-after_image_path = r"C:\Users\sbrown\Documents\GitHub\traction-force-microscopy\Data\C1-middle well002.png"
+before_image_path = r"C:\Users\sbrown\Documents\GitHub\traction-force-microscopy\Data\C1-middle well001_GS.bmp"
+after_image_path = r"C:\Users\sbrown\Documents\GitHub\traction-force-microscopy\Data\C1-middle well002_GS.bmp"
 before_image = plt.imread(before_image_path).astype(np.int32)
 after_image = plt.imread(after_image_path).astype(np.int32)
+both = cv2.add(before_image, after_image)
+plt.subplot(131)
+plt.imshow(before_image, cmap="gray")
+plt.title("Before")
+plt.subplot(132)
+plt.imshow(after_image, cmap="gray")
+plt.title("After")
+plt.subplot(133)
+plt.imshow(both, cmap="gray")
+plt.title("Overlay")
+plt.show()
 
-displacement_dict = get_displacements(before_image, after_image, 32, 64)
+displacement_dict = get_displacements(before_image, after_image, 100, 60)
 u, v = displacement_dict["u"], displacement_dict["v"]
-print(u)
-# tx, ty = reg_fttc(u, v, 1, 4000, 0.49, 0.12, False)
-
-
+show_quiver(u, v)
+plt.show()
+tx, ty = reg_fttc(u, v, 1, 4000, 0.49, 0.12, False)
+e_arr = strain_energy_points(u, v, tx, ty, 0.12)
+mask = plt.imread(r"C:\Users\sbrown\Documents\GitHub\traction-force-microscopy\Data\cells-middle well001_cp_masks.png")\
+    .astype(bool)
+plt.imshow(mask)
+plt.show()
 
