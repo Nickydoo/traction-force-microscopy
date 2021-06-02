886e3bf627048db2b171491294d6de0c290c65b8
Sam Brown <samuel.brown@alumni.ubc.ca>
2021-06-02 14:38:42 -0400

    Added check to make sure that the image is single-channel grayscale, changed noise threshold

--- TFM/DisplacementTracking.py
+++ TFM/DisplacementTracking.py
@@ -12,6 +12,8 @@ def get_displacements(img1, img2, window_size: int, overlap: int):
     :param window_size: size of search windows for cross correlation
     :return: dictionary with x,y coordinates of search windows and u (x-component) and v (y-component) of displacement
     """
+    if img1.ndim == 3 or img2.ndim == 3:
+        raise ValueError("Make sure to use a grayscale image!")
     u, v, sig2noise = extended_search_area_piv(img1, img2, window_size=window_size, overlap=overlap, dt=1,
                                                subpixel_method="gaussian", search_area_size=window_size,
                                                sig2noise_method="peak2peak")
@@ -21,13 +23,11 @@ def get_displacements(img1, img2, window_size: int, overlap: int):
     def_abs = np.sqrt(u ** 2 + v ** 2)
     m = np.nanmean(def_abs)
     std = np.nanstd(def_abs)
-    threshold = std * 1 + m
+    threshold = std * 10 + m
     mask_std = def_abs > threshold
     u[mask_std] = np.nan
     v[mask_std] = np.nan
-
     u, v = replace_outliers(u, v, method="localmean", max_iter=10, kernel_size=2)
     x, y = get_coordinates(image_size=img1.shape, window_size=window_size, overlap=overlap)
     y = y[::-1]
-    v = -v
     return {'x': x, 'y': y, 'u': u, 'v': v, 'mask': mask, 'mask_std': mask_std}
